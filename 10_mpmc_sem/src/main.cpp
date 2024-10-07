679d5c2d25892723a28dc73cd74e6b9d95c1a322
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-10-07 20:24:05 +0800

    Update main.cpp

--- 10_mpmc_sem/src/main.cpp
+++ 10_mpmc_sem/src/main.cpp
@@ -2,51 +2,56 @@
 // https://github.com/codophobia/producer-consumer-problem-solution-in-c/tree/master
 // https://github.com/codophobia/process-scheduling-algorithms/blob/master/roundrobin_scheduling.cpp
 
-// g++ -std=c++11 ./pc_2.cpp -lpthread && ./a.out
-
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include "logger.hpp"
 
 /*
-This program provides a possible solution for producer-consumer problem using mutex and semaphore.
-I have used 5 producers and 5 consumers to demonstrate the solution.
+  This program provides a possible solution for producer-consumer problem using mutex and semaphore.
+  5 producers and 5 consumers to demonstrate the solution.
 */
 
-#define MaxItems 5 // Maximum items a producer can produce or a consumer can consume
-#define BufferSize 5 // Size of the buffer
+#define MaxItems (5) // Maximum items a producer can produce or a consumer can consume
+#define BufferSize (5) // Size of the buffer
 
 sem_t empty;
 sem_t full;
 int in = 0;
 int out = 0;
-int buffer[BufferSize];
+uint64_t buffer[BufferSize]{0, 0, 0, 0, 0};
 pthread_mutex_t mutex;
+uint64_t item = 1; // Produce content, here just use a uint64_t
 
 void* producer(void* pno) {
-  int item;
-  for (int i = 0; i < MaxItems; i++) {
-    item = rand(); // Produce an random item
+  for (;;) {
     sem_wait(&empty);
+
     pthread_mutex_lock(&mutex);
     buffer[in] = item;
-    printf("Producer %d: Insert Item %d at %d\n", *((int*)pno), buffer[in], in);
+    LOGI(GREEN "Producer_%d: pushed %ld at %d", *((int*)pno), buffer[in], in);
     in = (in + 1) % BufferSize;
+    ++item;
     pthread_mutex_unlock(&mutex);
+
     sem_post(&full);
   }
+  return NULL;
 }
+
 void* consumer(void* cno) {
-  for (int i = 0; i < MaxItems; i++) {
+  for (;;) {
     sem_wait(&full);
+
     pthread_mutex_lock(&mutex);
-    int item = buffer[out];
-    printf("Consumer %d: Remove Item %d from %d\n", *((int*)cno), item, out);
+    auto item = buffer[out];
+    LOGI(RED "Consumer_%d: poped %ld from %d", *((int*)cno), item, out);
     out = (out + 1) % BufferSize;
     pthread_mutex_unlock(&mutex);
+
     sem_post(&empty);
   }
+  return NULL;
 }
 
 int main() {
@@ -57,17 +62,20 @@ int main() {
 
   int a[5] = {1, 2, 3, 4, 5}; // Just used for numbering the producer and consumer
 
-  for (int i = 0; i < 5; i++) {
-    pthread_create(&pro[i], NULL, (void*)producer, (void*)&a[i]);
+  int num_producer = 2;
+  int num_consumer = 2;
+
+  for (int i = 0; i < num_producer; i++) {
+    pthread_create(&pro[i], NULL, producer, (void*)&a[i]);
   }
-  for (int i = 0; i < 5; i++) {
-    pthread_create(&con[i], NULL, (void*)consumer, (void*)&a[i]);
+  for (int i = 0; i < num_consumer; i++) {
+    pthread_create(&con[i], NULL, consumer, (void*)&a[i]);
   }
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < num_producer; i++) {
     pthread_join(pro[i], NULL);
   }
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < num_consumer; i++) {
     pthread_join(con[i], NULL);
   }
 
